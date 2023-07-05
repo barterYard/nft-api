492b5642cf61957a917c1607afb7733107f4b221
Alexandre <alexandr.barbier@gmail.com>
2023-07-05 11:40:51 +0200

    Squashed 'rust_byc_helper/' content from commit 0001517
    git-subtree-dir: rust_byc_helper
    git-subtree-split: 00015170fb1b5401f83458412d492f6efc7aab56

---
+++ src/mongo/models/nft_api/nft.rs
@@ -0,0 +1,109 @@
+use crate::mongo::models::{common::ModelCollection, mongo_doc};
+use bson::oid::ObjectId;
+use mongodb::{error::Error, results::UpdateResult, Client};
+use proc::ModelCollection;
+use serde::{Deserialize, Serialize};
+
+use super::contract::Contract;
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, ModelCollection)]
+pub struct Nft {
+    pub _id: ObjectId,
+    pub id: String,
+    pub description: Option<String>,
+    pub name: Option<String>,
+    pub burned: bool,
+    pub contract_id: String,
+    pub contract: ObjectId,
+}
+
+impl Nft {
+    pub async fn insert(&self, client: &Client) {
+        let nft_col = Nft::get_collection(client);
+        match nft_col
+            .find_one(
+                mongo_doc! {"contract": self.contract, "id": self.id.clone()},
+                None,
+            )
+            .await
+        {
+            Ok(Some(x)) => {}
+            _ => {
+                let _ = nft_col.insert_one(self, None).await;
+            }
+        }
+    }
+
+    pub async fn get_or_create(
+        client: &Client,
+        contract: &Contract,
+        nft_id: String,
+        save: bool,
+    ) -> Nft {
+        let nft_col = Nft::get_collection(client);
+        match nft_col
+            .find_one(
+                mongo_doc! {"contract": contract._id, "id": nft_id.clone(), "burned": false},
+                None,
+            )
+            .await
+        {
+            Ok(y) => match y {
+                Some(nft) => return nft,
+                _ => {
+                    let new_nft = Nft {
+                        contract: contract._id,
+                        contract_id: contract.id.clone(),
+                        id: nft_id,
+                        _id: bson::oid::ObjectId::new(),
+                        ..Default::default()
+                    };
+                    if save {
+                        let _ = nft_col.insert_one(&new_nft, None).await;
+                    }
+                    new_nft
+                }
+            },
+            Err(_) => {
+                let new_nft = Nft {
+                    contract: contract._id,
+                    contract_id: contract.id.clone(),
+                    id: nft_id,
+                    _id: bson::oid::ObjectId::new(),
+                    ..Default::default()
+                };
+                if save {
+                    let _ = nft_col.insert_one(&new_nft, None).await;
+                }
+                new_nft
+            }
+        }
+    }
+
+    pub async fn burn(&self, client: &Client) -> Result<UpdateResult, Error> {
+        Nft::get_collection(client)
+            .update_one(
+                mongo_doc! {
+                    "_id": self._id
+                },
+                mongo_doc! {"$set": mongo_doc! {
+                    "burned": true,
+                }},
+                None,
+            )
+            .await
+    }
+    pub async fn mint(&self, client: &Client) -> Result<UpdateResult, Error> {
+        Nft::get_collection(client)
+            .update_one(
+                mongo_doc! {
+                    "_id": self._id
+                },
+                mongo_doc! {"$set": mongo_doc! {
+                    "burned": false,
+                }},
+                None,
+            )
+            .await
+    }
+}
---
+++ src/mongo/models/nft_api/transfert.rs
@@ -0,0 +1,91 @@
+use crate::mongo::models::{common::ModelCollection, mongo_doc};
+use bson::oid::ObjectId;
+use log::info;
+use mongodb::{error::Error, results::InsertOneResult, Client};
+use proc::ModelCollection;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, ModelCollection)]
+pub struct Transfert {
+    pub _id: ObjectId,
+    pub date: String,
+    pub from: String,
+    pub to: String,
+    pub nft: ObjectId,
+}
+
+impl Transfert {
+    pub fn new(date: String, from: String, to: String, nft: ObjectId) -> Self {
+        Transfert {
+            _id: ObjectId::new(),
+            date,
+            from,
+            to,
+            nft,
+        }
+    }
+
+    pub async fn create(
+        date: String,
+        from: String,
+        to: String,
+        nft: ObjectId,
+        client: &Client,
+    ) -> Result<InsertOneResult, Error> {
+        let transfer = Transfert {
+            _id: ObjectId::new(),
+            date,
+            from,
+            to,
+            nft,
+        };
+        Transfert::get_collection(client)
+            .insert_one(transfer, None)
+            .await
+    }
+
+    pub async fn get_or_create(
+        date: String,
+        from: String,
+        to: String,
+        nft: ObjectId,
+        client: &Client,
+    ) -> Option<(Transfert, bool)> {
+        let transfer_col = Transfert::get_collection(client);
+        println!("{:?}", nft.clone());
+        match transfer_col
+            .find_one(
+                mongo_doc! {
+                    "date": date.clone(),
+                    "from": from.clone(),
+                    "to": to.clone(),
+                    "nft": nft.clone()
+                },
+                None,
+            )
+            .await
+        {
+            Ok(Some(c)) => Some((c, false)),
+            _ => {
+                let transfer = Transfert {
+                    _id: ObjectId::new(),
+                    date,
+                    from,
+                    to,
+                    nft,
+                };
+                Transfert::get_collection(client)
+                    .insert_one(transfer.clone(), None)
+                    .await;
+
+                Some((transfer, true))
+            }
+        }
+    }
+
+    pub async fn save(&self, client: &Client) -> Result<InsertOneResult, Error> {
+        Transfert::get_collection(client)
+            .insert_one(self, None)
+            .await
+    }
+}
